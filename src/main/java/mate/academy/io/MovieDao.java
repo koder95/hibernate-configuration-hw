35e5fdcfde6c1ee92d1a68ba51a633e30703e4cc
Kamil Jan Mularski <kammul@outlook.com>
2025-11-03 16:59:35 +0100

    Add basic interfaces and exception
    Introduced MovieDao and MovieService interfaces for movie data access and service layers, respectively. Added DataProcessingException for handling runtime data processing errors.

---
+++ src/main/java/mate/academy/io/MovieDao.java
@@ -0,0 +1,13 @@
+package mate.academy.io;
+
+import java.util.Optional;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+
+@Dao
+public interface MovieDao {
+
+    Movie add(Movie movie);
+
+    Optional<Movie> get(Long id);
+}
